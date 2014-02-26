c6d04029054d3f7840ffbe4f638ea478230960ff
Honkarn <joeljansson89@gmail.com>
2014-02-26 21:00:33 +0100

    In memory compiling

--- PoQ/ClassGenerator/Program.cs
+++ PoQ/ClassGenerator/Program.cs
@@ -1,5 +1,7 @@
 ﻿using ClassGenerator.Interfaces;
+using Microsoft.CSharp;
 using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
@@ -31,18 +33,9 @@ namespace ClassGenerator
             AppDomain currentDomain = AppDomain.CurrentDomain;
             currentDomain.AssemblyLoad += new AssemblyLoadEventHandler(MyAssemblyLoadEventHandler);
 
-            string folder = AssemblyDirectory;
+            var csSourceCode = GimmeSnakeModule();
 
-            string sourcePath = Path.Combine(folder, "testClass.cs");
-            string targetPath = Path.Combine(folder, "test.dll");
-
-            if (File.Exists(targetPath))
-                File.Delete(targetPath);
-
-            File.WriteAllText(sourcePath, GimmeSnakeModule());
-            Process.Start(new ProcessStartInfo("cmd.exe", @"/c C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /t:library /r:System.dll,ClassGenerator.Interfaces.dll /out:" + targetPath + " " + sourcePath));
-
-            Assembly testDLL = Assembly.LoadFrom(targetPath);
+            Assembly testDLL = CSharpCodeCompile(csSourceCode, "testDLL");
 
             var moduleTypes = GetAllModules(testDLL);
 
@@ -56,10 +49,6 @@ namespace ClassGenerator
                     Console.WriteLine("My snake module runs at: AppleDesire " + plugin.AppleDesireCoefficient + " SnakeSafetyZone " + plugin.SnakeSafetyZoneWidth);
                 }
             }
-            
-            //var myType = testDLL.GetType("ClassGenerator.Plugin.TestPlugin");
-            //var methodName = myInterface.GetMembers().First().Name;
-            //var result = myInterface.InvokeMember(methodName, BindingFlags.InvokeMethod, null, obj, null);
         }
         static private IEnumerable<Type> GetAllModules(Assembly fromAssembly)
         {
@@ -113,6 +102,48 @@ namespace ClassGenerator
                 Console.WriteLine(a.FullName);
             }
             Console.WriteLine();
+        }
+
+        static Assembly DoUglyCSFileCompiling(string classContent)
+        {
+            string folder = AssemblyDirectory;
+
+            string sourcePath = Path.Combine(folder, "testClass.cs");
+            string targetPath = Path.Combine(folder, "test.dll");
+
+            if (File.Exists(targetPath))
+                File.Delete(targetPath);
+
+            File.WriteAllText(sourcePath, classContent);
+            Process.Start(new ProcessStartInfo("cmd.exe", @"/c C:\Windows\Microsoft.NET\Framework\v3.5\csc.exe /t:library /r:System.dll,ClassGenerator.Interfaces.dll /out:" + targetPath + " " + sourcePath));
+
+            return Assembly.LoadFrom(targetPath);
+        }
+
+        public static Assembly CSharpCodeCompile(string cSharpSourceCode, string assemblyName)
+        {
+            var provider = new CSharpCodeProvider(new Dictionary<string, string> { { "CompilerVersion", "v4.0" } });
+            var cp = new CompilerParameters();
+            cp.ReferencedAssemblies.Add("ClassGenerator.Interfaces.dll");
+            cp.GenerateExecutable = false;
+            cp.OutputAssembly = assemblyName + ".dll";
+            cp.GenerateInMemory = true;
+            CompilerResults cr = provider.CompileAssemblyFromSource(cp, cSharpSourceCode);
+            if (cr.Errors.Count > 0)
+            {
+                Console.WriteLine("Errors building {0} into {1}", cSharpSourceCode, cr.PathToAssembly);
+                foreach (CompilerError ce in cr.Errors)
+                {
+                    Console.WriteLine("  {0}", ce.ToString());
+                    Console.WriteLine();
+                }
+                throw new Exception("Compilation error");
+            }
+            else
+            {
+                Console.WriteLine("Source \n \n {0} \n \n \n built into {1} successfully.", cSharpSourceCode, cr.PathToAssembly);
+            }
+            return cr.CompiledAssembly;
         }
     }
 }
